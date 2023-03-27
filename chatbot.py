57c0eb3bbb07ff6227f08f71125bb65b4ec5fbac
pedroaltobelli23 <pedroaltobelli673@gmail.com>
2023-03-26 22:39:46 -0300

    crawl e search implementados

--- chatbot.py
+++ chatbot.py
@@ -3,15 +3,27 @@ import os
 import re
 from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
-from functions import pretty,address_get,MyHelp
+from functions import pretty_IP,address_get,MyHelp,inverted_index,search_words,web_scrapping,pretty_search,pretty_not_founded
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import shutil
+import pickle
+
 load_dotenv(find_dotenv())
 
 TOKEN = os.getenv('TOKEN')
+PATH = "db/cache"
+PATH_PICKLE = "db/pickle"
+FILE_PICKLE = PATH_PICKLE + "/parrot.pkl"
+CLASSIFIER_PICKLE = PATH_PICKLE + "/classifier.pkl"
 
 git_url = "https://github.com/pedroaltobelli23/NLP_chatbot"
 
+cache_names = dict()
+
 regexipv6 = "([0-9a-fA-F]{1,4}:){7,7}([0-9a-fA-F]{1,4})|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)"
-regexipv4 = "((([0-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-4][0-9])|(25[0-5]))\.){3}((([0-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-4][0-9])|(25[0-5])))"
+regexipv4 = "((([0-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-5]))\.){3}((([0-9])|([1-9][0-9])|(1[0-9][0-9])|(2[0-5][0-5])))"
 
 intents = discord.Intents.default()
 intents.members = True
@@ -23,9 +35,15 @@ bot.help_command = MyHelp()
 
 @bot.event
 async def on_ready():
+    # Se existir uma pasta chamada cache, ele apaga e cria outra.Tambem reseta a lista que esta guardada na memoria com o pickle
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+        os.mkdir(PATH_PICKLE)
+        # pickle.dump([], open(FILE_PICKLE, 'wb'))
+
     guild = discord.utils.get(bot.guilds, name='A Cidade dos Robôs')
     channel = discord.utils.get(guild.text_channels, name='bot-fest')
-    await channel.send('Hello world!\ntype !help for more information.')
+    await channel.send('Hello world!\nType !help for more information.')
 
 @bot.command(help="returns the source code from this bot")
 async def source(ctx):
@@ -49,21 +67,98 @@ async def run(ctx, ip,version="v4"):
         #versao v4
         if(re.fullmatch(regexipv4,ip)):
             infos = await address_get('http://ipwho.is/'+ip)
-            await ctx.send(embed=pretty(infos))
+            await ctx.send(embed=pretty_IP(infos))
         else:
             await ctx.send("Invalid IPv4")
     elif(re.fullmatch("v6",version)):
         #versao v6
         if(re.fullmatch(regexipv6,ip)):
             infos = await address_get('http://ipwho.is/'+ip)
-            await ctx.send(embed=pretty(infos))
+            await ctx.send(embed=pretty_IP(infos))
         else:
             await ctx.send("Invalid IPv6.")
     else:
         await ctx.send("Invalid version.See !help for more information.")
+
+@bot.command(help="web crawling from webpage")
+async def crawl(ctx,url):
+    #Pega url e faz o webscrapping dela. As paginas serao salvas na pasta cache e a lista com os nomes das urls no doc_names
+
+    nome_urls,jumped = web_scrapping(url=url,dump_path=PATH,max_l=10)
+
+    try:
+        if not os.path.isfile(FILE_PICKLE):
+            with open(FILE_PICKLE,"wb") as f:
+                cache_names = nome_urls
+                pickle.dump(nome_urls,f)
+        else:
+            with open(FILE_PICKLE,'rb') as f:
+                cache_names = pickle.load(f)
+            cache_names.update(nome_urls)
+            with open(FILE_PICKLE,"wb") as f:
+                pickle.dump(cache_names,f)
+        
+        total_data = []        
+        doc_names = []
+        
+        for filename,url_n in cache_names.items():
+            f = os.path.join(PATH, filename)
+            data = joblib.load(f,'r')
+            total_data.append(data)
+            doc_names.append(url_n)
+
+
+        with open(CLASSIFIER_PICKLE,"wb") as f:
+            classificador = inverted_index(total_data,doc_names)
+            pickle.dump(classificador,f)
+            
+    except Exception as e:
+        print(e)        
+    await ctx.send(f'successful web crawling from {url}')
     
+@bot.command(help="reset do web scrapping")
+async def reset(ctx):
+    shutil.rmtree(PATH)
+    shutil.rmtree(PATH_PICKLE)
+    
+    os.mkdir(PATH)
+    os.mkdir(PATH_PICKLE)
+    
+    await ctx.send("web scrapping de paginas antigas foram resetadas!")
+
+
+@bot.command(help="Seach for a word in the documents")
+async def search(ctx,*args):
+    words = list(args)
+    try:    
+        if os.path.isfile(CLASSIFIER_PICKLE):#classificador nao esta vazio
+            with open(CLASSIFIER_PICKLE,'rb') as f:
+                classificador = pickle.load(f)
+
+            res,not_founded_words,founded_words = search_words(words,classificador)
+            
+            if bool(res):#pelo menos uma palavra foi encontrada no classificador
+                await ctx.send(embed=pretty_search(res,founded_words))
+                
+            if bool(not_founded_words):#existem palavras que nao estao no classificador
+                await ctx.send(embed=pretty_not_founded(not_founded_words))
+        else:#nao existe classificador
+            await ctx.send(f"eh necessario usar o comando !crawl antes de buscar por palavras no classificador")
+    except Exception as e:
+        print(e)
+                
 @run.error
 async def run_error(ctx,error):
+    if isinstance(error,commands.MissingRequiredArgument):
+        await ctx.send("Missing required argument. See !help for more information.")
+        
+@crawl.error
+async def crawl_error(ctx,error):
+    if isinstance(error,commands.MissingRequiredArgument):
+        await ctx.send("Missing required argument. See !help for more information.")
+
+@search.error
+async def search_error(ctx,error):
     if isinstance(error,commands.MissingRequiredArgument):
         await ctx.send("Missing required argument. See !help for more information.")
 
--- functions.py
+++ functions.py
@@ -2,8 +2,45 @@ import aiohttp
 import asyncio
 import discord
 from discord.ext import commands
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import os
+import joblib
+import requests
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urljoin
 
-def pretty(info):
+def URL_validate(url):
+    re_url = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+    r = re.compile(re_url)
+
+    if (re.search(r, url)):
+        return True
+    return False
+
+def pretty_search(url_tdidf : dict,words : list):
+    title="Documentos contendo: "+" ".join(words)
+    embed = discord.Embed(title=title)
+    
+    for url,tfidf in url_tdidf.items():
+        embed.add_field(name=url,value=tfidf,inline=False)
+    
+    embed.color = discord.Color.dark_blue()
+    
+    return embed
+
+def pretty_not_founded(not_founded_words : list):
+    title="Nao encontradas do banco de dados"
+    embed = discord.Embed(title=title)
+    
+    for word in not_founded_words:
+        embed.add_field(name=word,value="",inline=False)
+    
+    embed.color = discord.Color.red()
+    return embed
+        
+def pretty_IP(info):
     embed = discord.Embed(title="IP finder")
     if(info['success']):
         embed.color = discord.Color.fuchsia()
@@ -46,3 +83,98 @@ class MyHelp(commands.HelpCommand):
         """If there is an error, send a embed containing the error."""
         channel = self.get_destination() # this defaults to the command context channel
         await channel.send(error)
+   
+def web_scrapping(url,dump_path,max_l=10):
+    # Parametros:
+    # url: url aonde sera feito o web scrapping
+    # dump_path: path para a pasta de cache
+    # max_l: max de requesicoes que podem ser feitas
+    # Retorna nada
+    jumped_urls = []
+    # Fazendo o web scrapping da url
+    nome_urls = dict()
+    next_urls = [url]
+    l=0
+    while next_urls:
+        url_now = next_urls[0]
+        if l<max_l:
+            
+            try:
+                page = requests.get(url=url_now,timeout=15)
+                #Twitter nao funciona muito bem
+                if page.status_code == 200 and re.search("^text/html",page.headers["Content-Type"]):
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    next_urls_tags = soup.find_all('a',href=True)
+                    # print(len(next_urls_tags))
+                    text = soup.get_text()
+                    clean_text = re.sub(r'[^\x00-\x7F]+', '', text)
+                    
+                    # Criacao do nome do arquivo aonde o texto sera guardado
+                    name = re.sub(r'[.:/]','',url_now)
+                    data_name = name + ".joblib"
+                    filename = os.path.join(dump_path,data_name)
+                    # print(url_now)
+                    # print(filename)
+                    # dump do texto contido na url em um arquivo .joblib. 
+                    # So 'e feito o dump se o arquivo nao existe
+                    if not os.path.exists(filename):
+                        with open(filename,'wb') as f:
+                            nome_urls[data_name] = url_now
+                            joblib.dump(clean_text,f)
+                            l+=1
+                            # print("foi")
+                    
+                    
+                    for a in next_urls_tags:
+                        
+                        link = a['href']
+                
+                        if URL_validate(link) is False:
+                            link = urljoin(url_now, link)                 
+                            
+                        next_urls.append(link)  
+
+            except Exception as e:
+                # print(f"algo deu errado,pulando essa url:{url_now}")
+                print(e)
+                jumped_urls.append(url_now)
+        
+        next_urls.pop(0)
+        
+    # dict_words = inverted_index(PATH,doc_names)
+    return nome_urls,jumped_urls   
+     
+def inverted_index(total_data,doc_names):
+
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform(total_data)
+
+    res = dict()
+    
+    for word in vectorizer.get_feature_names_out():
+        j = vectorizer.vocabulary_[word]
+        doc_df = dict()
+        for i in range(len(doc_names)):
+            if tfidf[i,j]>0:
+                doc_df[doc_names[i]] = tfidf[i,j]
+        res[word] = doc_df
+    
+    return res
+
+def search_words(palavras, indice):
+    assert type(palavras)==list
+    resultado = dict()
+    notfounded = []
+    founded_words = []
+    
+    for p in palavras:
+        if p in indice.keys():
+            founded_words.append(p)        
+            for documento in indice[p].keys():
+                if documento not in resultado.keys():
+                    resultado[documento] = indice[p][documento]
+                else:
+                    resultado[documento] += indice[p][documento]
+        else:
+            notfounded.append(p)
+    return resultado,notfounded,founded_words
